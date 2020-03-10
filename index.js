a3fc26960107af110082fba7d85c9eee52405f3c
Korti <lucas.korthauer@gmail.com>
2020-03-10 13:24:56 +0100

    Added main features.

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const core = require('@actions/core');
-const wait = require('./wait');
-
+const github = require('@actions/github')
 
 // most @actions toolkit packages have async methods
 async function run() {
+  if(!process.env.GITHUB_TOKEN) {
+    core.setFailed('GITHUB_TOKEN missing, must be set with ${{secrets.GITHUB_TOKEN}}');
+  }
   try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+    const issueNumber = core.getInput('issue');
+    const prefix = core.getInput('prefix');
+    const suffix = core.getInput('suffix');
+    const fillSymbol = core.getInput('fill-symbol');
+    const octokit = new github.Github(process.env.GITHUB_TOKEN);
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    const { data: issue } = await octokit.issue.get({
+      owner: owner,
+      repo: repo,
+      issue_number: issueNumber
+    });
 
-    core.setOutput('time', new Date().toTimeString());
+    const branchName = `${prefix}${issueNumber}-${issue.title}${suffix}`.replace(/[ ]+/g, fillSymbol)
+    core.setOutput('branch-name', branchName);
   } 
   catch (error) {
     core.setFailed(error.message);
